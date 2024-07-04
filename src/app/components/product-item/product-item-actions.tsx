cdfc1904dc09807b741d8b62155174bee2667b38
marcopetry <marcomattospetry@gmail.com>
2024-07-03 22:39:40 -0300

    feat: adjusts in style

--- src/app/components/product-item/product-item-actions.tsx
+++ src/app/components/product-item/product-item-actions.tsx
@@ -8,7 +8,8 @@ type ProductItemActionProps = {
 }
 
 export const ProductItemAction = ({ product }: ProductItemActionProps) => {
-  const { increaseItem, decreaseItem, getItemAmount, setAmount, items, addProduct } = useCarShop()
+  const { increaseItem, decreaseItem, getItemAmount, setAmount, addProduct } = useCarShop()
+
   const amountItem = getItemAmount(product.id)
   const handleAdd = () => {
     if (amountItem) {
@@ -18,12 +19,12 @@ export const ProductItemAction = ({ product }: ProductItemActionProps) => {
     }
   }
   return (
-    <div className="flex align-baseline justify-center mt-8 gap-5">
+    <div className="flex align-baseline justify-center gap-5">
       <button className="rounded-full p-2 bg-red-500" onClick={() => decreaseItem(product.id)}>
         <MdRemove size={24} />
       </button>
       <input
-        className="w-30 border rounded-md pl-4"
+        className="w-14 border rounded-md pl-4"
         value={amountItem}
         type="number"
         onChange={(e) => setAmount(product.id, Number(e.target.value))}
